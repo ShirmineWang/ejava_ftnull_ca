3712ef5d6b03e65a8888410a6cba47d5d0f4b059
LinBoYuan <boyuan_lin@yahoo.com>
2016-11-07 10:16:06 +0800

    initial commit

---
+++ week05_ca/src/java/sg/edu/nus/iss/javaee/epod/model/Pod.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.javaee.epod.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author linby
+ */
+@Entity
+public class Pod implements Serializable{
+    @Id
+    private int pod_id;
+    private int pkg_id;
+    private String note;
+    private byte[] image;
+    private Timestamp delivery_date;
+    private String act_id;
+
+    public int getPod_id() {
+        return pod_id;
+    }
+
+    public void setPod_id(int pod_id) {
+        this.pod_id = pod_id;
+    }
+
+    public int getPkg_id() {
+        return pkg_id;
+    }
+
+    public void setPkg_id(int pkg_id) {
+        this.pkg_id = pkg_id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Timestamp getDelivery_date() {
+        return delivery_date;
+    }
+
+    public void setDelivery_date(Timestamp delivery_date) {
+        this.delivery_date = delivery_date;
+    }
+
+    public String getAct_id() {
+        return act_id;
+    }
+
+    public void setAct_id(String act_id) {
+        this.act_id = act_id;
+    }
+
+}
---
+++ week05_ca/src/java/sg/edu/nus/iss/javaee/epod/model/delivery.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.javaee.epod.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author linby
+ */
+@Entity
+public class delivery implements Serializable {
+    @Id
+    private int pkg_id;
+    private String name;
+    private String adress;
+    private String phone;
+    private Timestamp create_date;
+
+    public int getPkg_id() {
+        return pkg_id;
+    }
+
+    public void setPkg_id(int pkg_id) {
+        this.pkg_id = pkg_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Timestamp getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Timestamp create_date) {
+        this.create_date = create_date;
+    }
+
+}
